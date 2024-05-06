c41d88f63a5dbada9be930930bcd11db570e6dff
mvvhello <mvvhello@gmail.com>
2024-05-06 15:21:48 -0300

    added and deleted:
    - NDSmallCategoryButton view (added)
    - NDCategoryCircle (added)
    
    - IncomeViewController (deleted)
    - NDButton (deleted)
    - NDTextField (deleted)

---
+++ Simpify/View/NDCategoryCircle.swift
@@ -0,0 +1,20 @@
+//
+//  NDCategoryCircle.swift
+//  Simpify
+//
+//  Created by Vlad on 06/05/2024.
+//
+
+import UIKit
+
+class NDCategoryCircle: UIView {
+
+    /*
+    // Only override draw() if you perform custom drawing.
+    // An empty implementation adversely affects performance during animation.
+    override func draw(_ rect: CGRect) {
+        // Drawing code
+    }
+    */
+
+}
---
+++ Simpify/View/NDSmallCategoryButton.swift
@@ -0,0 +1,20 @@
+//
+//  NDSmallCategoryButton.swift
+//  Simpify
+//
+//  Created by Vlad on 29/04/2024.
+//
+
+import UIKit
+
+class NDSmallCategoryButton: UIView {
+
+    /*
+    // Only override draw() if you perform custom drawing.
+    // An empty implementation adversely affects performance during animation.
+    override func draw(_ rect: CGRect) {
+        // Drawing code
+    }
+    */
+
+}
